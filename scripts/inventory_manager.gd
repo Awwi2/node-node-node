8b324b40ac7773e8d18bd1afa01461483a20d282
Awwi2 <l.behrendt@mtsl.de>
2026-06-14 14:44:36 +0200

    added hivemind and added inventory manager

--- scripts/inventory_manager.gd
+++ scripts/inventory_manager.gd
@@ -2,24 +2,28 @@ extends Node
 const DRAGGABLE_NODE = preload("uid://cuwehglyimia7")
 const RELAY_NODE = preload("uid://c6ipwji5grl0y")
 const PLUS_NODE = preload("uid://e0dh5t3j38u5")
-
-
+const HIVEMIND_NODE = preload("uid://cqktm02qtvuql")
+var g_container
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	g_container = get_tree().root.get_node("/root/backpack/ScrollContainer/MarginContainer/GridContainer")
+	
+	add_node(RELAY_NODE, [Vector2i.RIGHT, Vector2i.LEFT, 
+		Vector2i.UP, Vector2i.DOWN])
+	
+	add_node(PLUS_NODE, [Vector2i.RIGHT, Vector2i.LEFT, 
+		Vector2i.UP, Vector2i.DOWN])
+	
+	add_node(HIVEMIND_NODE, [Vector2i.RIGHT, Vector2i.LEFT])
+	
+	add_node(HIVEMIND_NODE, [Vector2i.RIGHT, Vector2i.LEFT])
+
+func add_node(n, ports: Array[Vector2i]) -> void:
 	var d1:DraggableNode = DRAGGABLE_NODE.instantiate()
-	var new_node:Nodey =  RELAY_NODE.instantiate()
-	new_node.ports = [Vector2i.RIGHT, Vector2i.LEFT, 
-		Vector2i.UP, Vector2i.DOWN]
+	var new_node:Nodey =  n.instantiate()
+	new_node.ports = ports
 	d1.attached_node = new_node
 	d1.add_child(new_node)
-	var g_container = get_tree().root.get_node("/root/backpack/ScrollContainer/MarginContainer/GridContainer")
 	g_container.add_child(d1)
 	
-	var d2: DraggableNode = DRAGGABLE_NODE.instantiate()
-	var newer_node: Nodey =PLUS_NODE.instantiate()
-	newer_node.ports = [Vector2i.RIGHT, Vector2i.LEFT, 
-		Vector2i.UP, Vector2i.DOWN]
-	d2.attached_node = newer_node
-	d2.add_child(newer_node)
-	g_container.add_child(d2)
